9468430c57fafd6f46bcf159885acbeba769cff9
Murat <muratnysambaev93@gmail.com>
2019-05-15 21:34:24 +0600

    lesson-1

---
+++ backend/controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace backend\controllers;
+
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function actionTest()
+    {
+        return 'Hello world';
+    }
+}
---
+++ console/migrations/m190514_175123_create_column_avatar.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190514_175123_create_column_avatar
+ */
+class m190514_175123_create_column_avatar extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->addColumn('{{%user}}', 'access_token', $this->string('255')->defaultValue(null));
+        $this->addColumn('{{%user}}', 'avatar', $this->string('255')->defaultValue(null));
+    }
+
+    public function down()
+    {
+        $this->dropColumn('{{%user}}', 'access_token');
+        $this->dropColumn('{{%user}}', 'avatar');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190514_175123_create_column_avatar cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
